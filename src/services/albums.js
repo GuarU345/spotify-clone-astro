954c336ce078392073b712fab998431ce62063ea
guaru345 <abelmtz34@outlook.com>
2023-11-06 10:44:56 -0600

    start main component

--- src/services/albums.js
+++ src/services/albums.js
@@ -1,7 +1,7 @@
 export const getAlbums = async () => {
   const res = await fetch("http://localhost:3000/api/albums", {
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjRjZjVlYTlmLTAxMTEtNDdmZi05NzRjLTBkNGU2ZDcxNmUwOCIsImVtYWlsIjoiYWJlbEBnbWFpbC5jb20iLCJpYXQiOjE2OTg3OTUzNzN9.qFNJiXNrTPyP5-2pOieOqSAzDqCXIzxmm8CM0gv_WwI`,
+      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImUxOWYxYzEwLWM1ZWQtNDkyMS05MmZlLTc5ZTM4NGJlMDI3OCIsImVtYWlsIjoiYWJlbEBnbWFpbC5jb20iLCJpYXQiOjE2OTkyODA4MDV9.v-365CZyEG5_ImP5HlrnVIQnLUdu9AOpLAs2n1FZIBA`,
     },
   });
   const albums = await res.json();
