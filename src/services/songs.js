6ae3d8fd91ff7b63c6587187ed063def7c6d1a68
GuarU345 <95324317+GuarU345@users.noreply.github.com>
2023-11-21 00:44:24 -0600

    add token in a services

--- src/services/songs.js
+++ src/services/songs.js
@@ -1,7 +1,22 @@
 import { API_URL } from "../utils/helpers";
 
 export const getSongById = async (songId) => {
-  const res = await fetch(`${API_URL}/songs/${songId}`);
+  const res = await fetch(`${API_URL}/songs/${songId}`, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM5ZGUyZThlLTRjYjQtNGQyNi1hMjBhLTU0YjFkNDIzYWRkNSIsImVtYWlsIjoiZ3VhcnVAZ21haWwuY29tIiwiaWF0IjoxNjk5MjQ2Nzc1fQ.Qce5869LNnTt_sdUghlPgdQQZg0RRLkBSnMq0BkK_Ow",
+    },
+  });
   const song = await res.json();
   return song;
+};
+
+export const streamSong = async (songId) => {
+  const { body } = await fetch(`${API_URL}/songs/${songId}/stream`, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjM5ZGUyZThlLTRjYjQtNGQyNi1hMjBhLTU0YjFkNDIzYWRkNSIsImVtYWlsIjoiZ3VhcnVAZ21haWwuY29tIiwiaWF0IjoxNjk5MjQ2Nzc1fQ.Qce5869LNnTt_sdUghlPgdQQZg0RRLkBSnMq0BkK_Ow",
+    },
+  });
+  const bytes = await body.getReader();
 };
