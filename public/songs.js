a3b874e42b7b7729f8e451e0011c54a8842d3c92
GuarU345 <95324317+GuarU345@users.noreply.github.com>
2023-11-21 00:36:58 -0600

    add songs in a json and the file of songs

---
+++ public/songs.js
@@ -0,0 +1,22 @@
+export const STREAMSONGS = [
+  {
+    name: "BLOOD",
+    track: "songs/BLOOD.mp3",
+  },
+  {
+    name: "DNA",
+    track: "songs/DNA.mp3",
+  },
+  {
+    name: "ELEMENT",
+    track: "songs/ELEMENT.mp3",
+  },
+  {
+    name: "FEEL",
+    track: "songs/FEEL.mp3",
+  },
+  {
+    name: "DUCKWORTH",
+    track: "songs/DUCKWORTH.mp3",
+  },
+];
