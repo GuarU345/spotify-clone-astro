cdba0d3c7f1af579dceab5754231b337ad828654
guaru345 <abelmtz34@outlook.com>
2023-11-23 09:18:42 -0600

    add 2 services to like and dislike song and component like

---
+++ src/components/react/Like.jsx
@@ -0,0 +1,32 @@
+import { BsHeart, BsHeartFill } from "react-icons/bs";
+import { dislikeSong, likeSong } from "src/services/user_actions";
+
+const Like = ({ liked, songId }) => {
+  const handleLike = async () => {
+    if (liked === false) {
+      try {
+        await likeSong(songId);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      try {
+        dislike = await dislikeSong(songId);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+  return (
+    <>
+      <button
+        onClick={handleLike}
+        className={`${liked ? "text-green-500" : "d-none"} text-lg`}
+      >
+        {liked ? <BsHeartFill /> : <BsHeart />}
+      </button>
+    </>
+  );
+};
+
+export default Like;
---
+++ src/services/user_actions.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { API_URL, token, userId } from "src/utils/helpers";
+
+export const likeSong = async (songId) => {
+  const { data } = await axios.post(
+    `${API_URL}/users/${userId}/songs/${songId}/likes`,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return data;
+};
+
+export const dislikeSong = async (songId) => {
+  const { data } = await axios.delete(
+    `${API_URL}/users/${userId}/songs/${songId}/likes`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+  return data;
+};
